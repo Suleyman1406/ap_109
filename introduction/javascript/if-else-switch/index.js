c102aad9e0ec0c18061bef554914185812f85413
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-03-06 10:04:35 +0400

    if else lesson added

---
+++ introduction/javascript/if-else-switch/index.js
@@ -0,0 +1,36 @@
+let mehemmedsAge = 75;
+
+// if(mehemmedsAge >= 65){
+//   console.log('ged basqa yere');
+// }
+// else if(mehemmedsAge >= 18){
+//   console.log('Giris edildi');
+// }
+// else{
+//   console.log('Giris edilmedi');
+//   console.log('Giris edilmedi');
+//   console.log('Giris edilmedi');
+//   console.log('Giris edilmedi');
+//   console.log('Giris edilmedi');
+//   console.log('Giris edilmedi');
+// }
+
+// if(mehemmedsAge >= 18 && mehemmedsAge<65){
+//   console.log('Giris edildi');
+// }
+// else if(mehemmedsAge >= 65){
+//   console.log('ged basqa yere');
+// }
+// else if(mehemmedsAge >= 72){
+//   console.log('ged eve');
+// }
+// else{
+//   console.log('Giris edilmedi');
+//   console.log('Giris edilmedi');
+// }
+
+let checked = false;
+
+if (checked || (true && 66 * 266 > 56 * 275)) {
+  console.log("goruldu");
+}
