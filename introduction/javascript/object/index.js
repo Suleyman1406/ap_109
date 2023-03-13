2031c5740c0626a68568dc2c252ea772f9642462
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-03-13 09:41:05 +0400

    object lesson added

---
+++ introduction/javascript/object/index.js
@@ -0,0 +1,35 @@
+// let car = {
+//   "5-if": "BMW",
+//   if: true,
+//   price: 12,
+//   colours: ["yellow", "pink"],
+//   seller: {
+//     name: "Mehemmed",
+//     surname: "Eliyev",
+//     age: 12,
+//   },
+// };
+
+// console.log(car["5-if"]);
+// console.log(car.if);
+// console.log(car.price);
+// console.log(car.colours);
+// console.log(car.seller.name);
+
+// let car = {
+//   "5-if": "BMW",
+//   price: 12,
+//   colours: ["yellow", "pink"],
+//   seller: {
+//     // name: "Mehemmed",
+//     surname: "Eliyev",
+//     age: 12,
+//   },
+// };
+// if (car.seller) console.log(car.seller.name);
+
+// let a = false;
+// let b = false;
+// if (!(a && !b)) {
+//   console.log("eltun");
+// } else console.log("mehemmed");
