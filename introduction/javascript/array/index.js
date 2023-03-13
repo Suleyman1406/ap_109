50ef8346884b9998e42aa70a9b62847022bac2a2
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-03-13 08:52:54 +0400

    array lesson added

---
+++ introduction/javascript/array/index.js
@@ -0,0 +1,12 @@
+// let myArray = [];
+// myArray.push(2);
+// myArray.push(8);
+// myArray.push(true);
+// myArray.push("2");
+// console.log(myArray);
+// myArray.unshift("s");
+// console.log(myArray);
+// myArray.pop();
+// console.log(myArray);
+// myArray.shift();
+// console.log(myArray);
